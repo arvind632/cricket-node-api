54aaffb31a44be06404e3cf385eb0d5ae7fc5d39
arvind <arvindsingh632@gmail.com>
2020-05-16 20:08:42 +0530

    update new changes

--- routes/cricket.js
+++ routes/cricket.js
@@ -8,13 +8,16 @@ exports.team = (req, res) => {
   });
 };
 
+// Fetch player Api
 exports.player = (req, res) => {
+  
   db.query('select * from player', (error,result)=>{
     if(error) throw error;
     res.send(result);
   });
-};
+  
 
+};
 
 exports.history = (req, res) => {
   db.query('SELECT player.firstName,player.lastName, history.* FROM history LEFT JOIN player ON player.id=history.player_id', (error,result)=>{
@@ -22,6 +25,99 @@ exports.history = (req, res) => {
     res.send(result);
   });
 };
+
+
+// Create player Api
+exports.playerCreate = (req, res) => {
+  // Validate request
+  if (!req.body.firstName){
+      res.status(401).send({
+        success:false,
+        message: "First name is required"
+      });
+      return;
+  }
+  else if(!req.body.lastName){
+    res.status(401).send({
+      success:false,
+      message: "Last name is required"
+    });
+    return;
+  }
+  
+    else{
+      var player = {
+            firstName : req.body.firstName,
+            LastName : req.body.lastName,
+            imageUri : req.body.imageUri,
+            PlayerJerseyNumber : req.body.PlayerJerseyNumber
+          }
+    }
+
+    // Save player in the database
+    db.query('insert into player SET ?', player, (error,result,fields)=>{
+        if(error){
+        res.status(400).send({
+          success:false,
+          failed:error
+        })
+0      }else{
+        res.status(200).send({
+          success:true,
+          message:"Player created successfully"
+        });
+      }
+    })
+  
+}
+
+
+// Find a single record with an id
+exports.playerFindById = (req, res ) => {
+  const id = req.params.id;
+  db.query("select * from player where id =? ",id,(error,result)=>{
+    if(error){
+      res.status(400).send({
+        success:false,
+        message:error
+      });
+      
+    }else{
+      res.status(200).send(result);
+    }
+  })
+  
+}
+
+
+// update player
+exports.playerUpdate = (req, res) => {
+  const _id = req.params.id;
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var PlayerJerseyNumber = req.body.PlayerJerseyNumber;
+  db.query("update player SET firstName='"+firstName+"', lastName='"+lastName+"', PlayerJerseyNumber='"+PlayerJerseyNumber+"' where id = ? ",+ _id, (error,result,fields) => {
+      if(error){
+        res.status(401).send({
+            success:false,
+            message:error
+        });
+      }else{
+        res.status(200).send({
+          success:true,
+          message:"record update successfully",
+          result:result.affectedRows
+        });
+      }
+  });
+}
+
+
+
+
+
+
+
 
 
 
--- server.js
+++ server.js
@@ -3,8 +3,18 @@ var cricket = require('./routes/cricket');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var path = require('path');
-
+const cors = require("cors");
 var app = express();
+
+const errorHandler = require('./_helpers/error-handler');
+
+
+var corsOptions = {
+    origin: "http://localhost:1000"
+};
+
+app.use(cors(corsOptions));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -14,12 +24,13 @@ app.use(session({
 	saveUninitialized: true
 }));
 
-
+/*
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+*/
 
 app.get('/', function(req, res) {
     res.json({ message: 'welcome to our upload module apis' });
@@ -31,5 +42,13 @@ app.get('/', function(req, res) {
 // Api Route
 app.get('/api/team',cricket.team);
 app.get('/api/player',cricket.player);
+app.post('/api/create',cricket.playerCreate);
+app.get('/api/player/:id',cricket.playerFindById);
+app.put('/api/player/:id',cricket.playerUpdate);
+
 app.get('/api/history',cricket.history);
+
+// global error handler
+app.use(errorHandler);
+
 app.listen(5000);
